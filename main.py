7249d459b7494f30b63ce8528cde7c9f823c7f43
PedroGirotto <pedro.hs.girotto@gmail.com>
2025-07-17 10:57:12 -0300

    Implementando a 2º e 3º linha do FrontEnd do dashboard

--- main.py
+++ main.py
@@ -62,8 +62,8 @@ app.layout = dbc.Container(children=[
             dbc.Card([
                 dbc.CardBody([
                     dbc.Row([
+                        html.H3("Máximos e Mínimos"),
                         dbc.Col([
-                            html.H3("Máximos e Mínimos"),
                             dcc.Graph(id="static-maxmin", config={"displayModeBar":False, "showTips": False})
                         ])
                     ])
@@ -100,19 +100,145 @@ app.layout = dbc.Container(children=[
                         ], sm=6)
                     ]),
 
+                    #! gráficos não ficam na mesma linha
                     dbc.Row([
                         dbc.Col([
                             dcc.Graph(id="regiao_graph", config={"displayModeBar":False, "showTips":False})
-                        ], sm=12, md=6),
+                        ], sm=6),
 
                         dbc.Col([
                             dcc.Graph(id="estado_graph", config={"displayModeBar":False, "showTips":False})
-                        ], sm=12, md=6)
-                    ], style={"column-gap":"8px"})
+                        ], sm=6)
+                    ], style={"column-gap":"4px"})
                 ])
             ], style=tab_card)
         ], sm=12, lg=7)
-    ]) 
+    ], class_name="g-2 my-auto"),
+
+
+    dbc.Row([ # linha 02
+        dbc.Col([ # coluna 01
+            dbc.Card([
+                dbc.CardBody([
+                    html.H3("Preço x Estado"),
+                    html.H6("Comparação temporal entre estados"),
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Dropdown(
+                                id="select_estados0",
+                                value=[df.at[df.index[3], "ESTADO"], df.at[df.index[6], "ESTADO"], df.at[df.index[13], "ESTADO"]],
+                                clearable=False,
+                                className="dbc",
+                                multi=True,
+                                options=[
+                                    {"label":x, "value":x} for x in df["ESTADO"].unique()
+                            ])
+                        ], sm=10)
+                    ]),
+
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Graph(id="animation_graph", config={"displayModeBar":False, "showTips":False})
+                        ])
+                    ])
+                ])
+            ], style=tab_card)
+        ], sm=12, md=6, lg=5),
+
+        dbc.Col([ # coluna 02
+            dbc.Card([
+                dbc.CardBody([
+                    html.H3("Comparação Direta"),
+                    html.H6("Qual preço é menor em um dado período de tempo?"),
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Dropdown(
+                                id="select_estado1",
+                                value=df.at[df.index[3], "ESTADO"],
+                                clearable=False,
+                                className="dbc",
+                                options=[
+                                    {"label":x, "value":x} for x in df["ESTADO"].unique()
+                            ])
+                        ], sm=10, md=5),
+
+                        dbc.Col([
+                            dcc.Dropdown(
+                                id="select_estado2",
+                                value=df.at[df.index[1], "ESTADO"],
+                                clearable=False,
+                                className="dbc",
+                                options=[
+                                    {"label":x, "value":x} for x in df["ESTADO"].unique()
+                            ])
+                        ], sm=10, md=5),
+                    ], style={"margin-top":"20px"}, justify="center"),
+
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Graph(id="direct_comparison_graph", config={"displayModeBar":False, "showTips":False}),
+                        ])
+                    ]),
+                    html.P(id="desc_comparison", style={"color":"gray", "font-size":"80%"}),
+                ])
+            ], style=tab_card)
+        ], sm=12, md=6, lg=4),
+
+        dbc.Col([ # coluna 03
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id="card1_indicators", config={"displayModeBar":False, "showTips": False}, style={"margin-top": "30px"})
+                        ])
+                    ], style=tab_card)
+                ])
+            ], justify="center", style={"padding-bottom":"7px", "height":"50%"}),
+
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id="card2_indicators", config={"displayModeBar":False, "showTips": False}, style={"margin-top": "30px"})
+                        ])
+                    ], style=tab_card)
+                ])
+            ], justify="center", style={"height":"50%"})
+        ], sm=12, lg=3, style={"height":"100%"})
+    ], className="g-2 my-auto"),
+
+
+    dbc.Row([ # linha 03
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Button([html.I(className="fa fa-play")], id="play-button", style={"margin-right":"15px"}),
+                            dbc.Button([html.I(className="fa fa-stop")], id="stop-button")
+                        ], sm=12, md=1, style={"justify-content":"center", "margin-top":"10px"}),
+
+                        dbc.Col([
+                            dcc.RangeSlider(
+                                id="ranger_slider",
+                                marks={int(x): f"{x}" for x in df["ANO"].unique()},
+                                step=3,
+                                min=df["ANO"].min(),
+                                max=df["ANO"].max(),
+                                className="dbc",
+                                value=[df["ANO"].min(),df["ANO"].max()],
+                                dots=True,
+                                pushable=3,
+                                tooltip={"always_visible":False, "placement":"bottom"},
+                            )
+                        ], sm=12, md=10, style={"margin-top":"15px"}),
+
+                        dcc.Interval(id="interval", interval=2000),
+                    ], className="g-1", style={"height":"20%", "justify-content":"center"})
+                ])
+            ], style=tab_card)
+        ])
+    ], className="g-2 my-auto")
 ], fluid=True, style={"height":"100%"})
 
 
